eeefeda7b0edc936da8a1684f6979adbe5a265ce
aakashm1712 <mukherjeea1@wit.edu>
2024-04-17 14:17:30 -0400

    Removed Local Trip Storage

--- src/main/java/edu/wit/cs/comp1050/javafx_maven_example/SmartCartController.java
+++ src/main/java/edu/wit/cs/comp1050/javafx_maven_example/SmartCartController.java
@@ -38,35 +38,47 @@ public class SmartCartController implements Initializable{
 
     @Override
 	public void initialize(URL location, ResourceBundle resources) {
-    	savedTrips = loadTripsFromFile("/comp1050-s24-projects-smart-cart/src/main/resources/edu/wit/cs/comp1050/javafx_maven_example/savedTrips.txt");
-    	pastTrips = loadTripsFromFile("/comp1050-s24-projects-smart-cart/src/main/resources/edu/wit/cs/comp1050/javafx_maven_example/pastTrips.txt");
-        populatetreeview();
+    	initializePreloadedTrips();
+    	populatetreeview();
 	}
     
-    private HashMap<String, ArrayList<String>> loadTripsFromFile(String fileName) {
-    	
-        HashMap<String, ArrayList<String>> data = new HashMap<>();
+    private void initializePreloadedTrips() {
+        savedTrips = new HashMap<>();
         
-        try (Scanner scanner = new Scanner(new File(fileName))) {
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                String[] parts = line.split(",");
-                String category = parts[0];
-                
-                ArrayList<String> items = new ArrayList<>();
-                
-                for (int i = 1; i < parts.length; i++) {
-                    items.add(parts[i].trim());
-                }
-                
-                data.put(category, items);
-            }
-        } catch (IOException e) {
-            System.out.printf("%nCant open the file: %s%n", fileName);
-            System.exit(0);
-        }
+        ArrayList<String> st1 = new ArrayList<>();
+        st1.add("Tostitos chips");
+        st1.add("Chip Dip");
+        st1.add("2-liter sodas"); 
+        st1.add("Disposable Cutlery"); 
+        st1.add("Cake"); 
+        savedTrips.put("Hosting Party", st1);
         
-        return data;
+        ArrayList<String> st2 = new ArrayList<>();
+        st2.add("Peppers");
+        st2.add("Onions");
+        st2.add("Chicken Breast"); 
+        st2.add("Spices"); 
+        st2.add("Rice"); 
+        savedTrips.put("Meal Prep", st2);
+        
+        
+    	pastTrips = new HashMap<>();
+
+        ArrayList<String> pt1 = new ArrayList<>();
+        pt1.add("Mangos");
+        pt1.add("Apples");
+        pt1.add("Honey"); 
+        pt1.add("Napkins"); 
+        pt1.add("Ice Cream"); 
+        pastTrips.put("2024-04-01", pt1);
+        
+        ArrayList<String> pt2 = new ArrayList<>();
+        pt2.add("Peppers");
+        pt2.add("Onions");
+        pt2.add("Pringles"); 
+        pt2.add("Pasta") ; 
+        pt2.add("Alfredo sauce"); 
+        pastTrips.put("2024-04-11", pt2);
     }
 
     private void populatetreeview() {
@@ -177,8 +189,6 @@ public class SmartCartController implements Initializable{
 				TreeItem<String> newItem = new TreeItem<>(item); 
 				todayCategory.getChildren().add(newItem); 
 			}
-			savedTrips.put(dialog.getResult(), new ArrayList<>(MLlistview.getItems()));
-            saveToFile(savedTrips, "savedTrips.txt");
 			MLlistview.getItems().clear(); 
 		}
 	}
@@ -197,29 +207,7 @@ public class SmartCartController implements Initializable{
 				TreeItem<String> newItem = new TreeItem<>(item); 
 				todayCategory.getChildren().add(newItem); 
 			}
-			pastTrips.put(today.toString(), new ArrayList<>(MLlistview.getItems()));
-	        saveToFile(pastTrips, "pastTrips.txt");
 			MLlistview.getItems().clear();
 		}
 	}
-	
-	
-    // Code runs but changes dont show up in txt file though it shows up on github -> ?????
-	// Need to find out why and fix ASAP
-	private void saveToFile(HashMap<String, ArrayList<String>> trips, String fileName) {
-	    try (FileWriter writer = new FileWriter(fileName)) {
-	        for (String category : trips.keySet()) {
-	            writer.write(category);
-	            System.out.printf("%n%s: ", category);
-	            for (String item : trips.get(category)) {
-	                writer.write("," + item);
-	                System.out.printf("%s, ", item);
-	            }
-	            writer.write("\n");
-	        }
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	    }
-	}
-
 }
